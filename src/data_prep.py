721b08134aff2be1b301cdd72262aa1b05fe71db
Febian Febian <febianfeb123@gmail.com>
2024-03-13 19:21:43 +0000

    Added basic functions to prepared data

---
+++ src/data_prep.py
@@ -0,0 +1,17 @@
+import pandas as pd
+import numpy as np
+
+def data_date(data):
+    date = np.array(data['Date'])
+    return date
+
+def data_closing_price(data):
+    closing_price = np.array(data['Close/Last'])
+    return closing_price
+
+def float_price(closing_price):
+    period_length = np.arange(1,len(closing_price)+1)
+
+    for i in range(len(closing_price)):
+        closing_price[i] = float(closing_price[i].replace('$', ''))
+    return closing_price, period_length
