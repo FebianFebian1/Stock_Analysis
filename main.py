e4aa4c01d84248dc432d7126df1a364d3b614641
Febian Febian <febianfeb123@gmail.com>
2024-03-13 19:21:56 +0000

    Main analysis file

---
+++ main.py
@@ -0,0 +1,28 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from src.data_prep import float_price, data_date, data_closing_price
+
+# Load the data
+data = pd.read_csv('src/HistData5Y_walmart.csv')
+
+#collect the date column and the closing price column
+date = data_date(data)
+closing_price = data_closing_price(data)
+
+#convert the closing price to a float
+closing_price, period_length = float_price(closing_price)
+
+#plot the data
+plt.figure()
+plt.plot(period_length[100:1000], closing_price[100:1000])
+plt.xlabel('Date')
+plt.ylabel('Closing Price')
+plt.title('Walmart Stock Price')
+
+# fft closing price
+plt.figure()
+closing_price_fft = np.fft.fft(closing_price)
+closing_price_fft = np.abs(closing_price_fft)
+plt.plot(period_length[100:1000], closing_price_fft[100:1000])
+plt.show()  
