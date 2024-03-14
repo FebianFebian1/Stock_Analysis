58658adb80f9749c2c27c64d589c7ab7e3374a49
Febian Febian <febianfeb123@gmail.com>
2024-03-14 18:37:05 +0000

    Linear Regression Implementation

--- main.py
+++ main.py
@@ -20,9 +20,8 @@ closing_price = data_closing_price(data)
 volume = data_volume(data)
 
 #convert the closing price to a float
-closing_price, period_length = float_price(closing_price)
+closing_price = float_price(closing_price)
 numeric_date = data_numeric_date(date)
-
 
 #plot the data
 plt.figure()
@@ -31,6 +30,23 @@ plt.xlabel('Date')
 plt.ylabel('Closing Price')
 plt.title('Walmart Stock Price')
 plt.show()
+
+# Linear Regression Implementation
+# Reshape the data
+start_date = min(numeric_date)
+numeric_dates = [(d - start_date).days for d in numeric_date]
+numeric_dates = np.array(numeric_dates).reshape(-1, 1)
+closing_price = np.array(closing_price).reshape(-1, 1)
+
+# split the data
+X_train, X_test, y_train, y_test = train_test_split(numeric_dates, closing_price, test_size=0.2, random_state=0)
+
+# Initialize and fit the linear regression model
+model = LinearRegression()
+model.fit(X_train, y_train)
+
+# Make predictions
+y_pred = model.predict(X_test)
 
 # fft closing price
 # plt.figure()
