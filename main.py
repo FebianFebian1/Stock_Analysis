c28d7682b995c3f4a29195c510f6925435af3b64
Febian Febian <febianfeb123@gmail.com>
2024-08-19 23:07:45 +0100

    Updated functionality using Yfinance

--- main.py
+++ main.py
@@ -1,17 +1,20 @@
-import pandas as pd
-
-from src.data_prep import Data_prep
-from src.StockLinearRegression import ML_analysis
+from src.stock_data import StockData
+from src.stock_linear_regression import ML_analysis
 
 
-# Load the data
+
+# # Load the data from donwloaded csv file from NASDAQ
 # data = pd.read_csv('src/csv_files/HistData5Y_walmart.csv')
-data = pd.read_csv('src/csv_files/HistData5Y_apple.csv')
+# data = pd.read_csv('src/csv_files/HistData5Y_apple.csv')
 
-# Plot the stock trend
-stock = Data_prep(data)
-stock.price_date()
+# # Plot the stock trend
+# stock = Data_prep(data)
+# stock.price_date()
+
+ticker = 'AAPL'
+data = StockData(ticker)
+data.price_date()
 
 # Linear Regression
-stock_analysis = ML_analysis(data)
+stock_analysis = ML_analysis(ticker)
 stock_analysis.StockLinearRegression()
